d5fd73c1b3964d6105154e4117c7ad033df3a5c3
OTheMagic <yunjie500@ucsb.edu>
2023-02-20 18:34:01 -0800

    Delete ObjectTrackingInterface.py

--- data/ObjectTrackingInterface.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import numpy as np
-import math
-import matplotlib.pyplot as plt
-import utility
-
-video = input("Please include the local path to the video: ")
-background = input("Please include the local path to the background of the above video: ")
-
